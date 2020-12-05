f125bcbf7be311137b71154acaa4830439d6d985
Hayk <hayk.hayr3@gmail.com>
2020-12-05 21:46:21 +0400

    Add project files.

---
+++ Less03_task02/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Less03_task02
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			new Derived(1, 2, 3);
+		}
+	}
+
+	class Base
+	{
+		public int x;
+		public int y;
+		public Base(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	class Derived : Base
+	{
+		public int z;
+		public Derived(int x, int y, int z) : base(x, y)
+		{
+			this.z = z;
+		}
+	}
+}
